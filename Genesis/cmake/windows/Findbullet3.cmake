fab3e93a325b05df57a33da6f690c1a88f4535b9
Pedro Nunes <pnunes515@protonmail.com>
2022-05-19 21:51:25 +0100

    Added Release config. Fixed up FindBullet3.cmake.

--- Genesis/cmake/windows/Findbullet3.cmake
+++ Genesis/cmake/windows/Findbullet3.cmake
@@ -1,5 +1,5 @@
 set(BULLET3_INCLUDE_DIRS "${CMAKE_SOURCE_DIR}/Genesis/libs/bullet3-2.87/include" CACHE STRING "Bullet3 - include directory")
-set(BULLET3_LIBRARY_DIRS "${CMAKE_SOURCE_DIR}/Genesis/libs/bullet3-2.87/lib/win32" CACHE STRING "Bullet3 - library directory")
+set(BULLET3_LIBRARY_DIRS "${CMAKE_SOURCE_DIR}/Genesis/libs/bullet3-2.87/lib/win32/$<$<CONFIG:Debug>:debug>$<$<CONFIG:RelWithDebInfo>:release>" CACHE STRING "Bullet3 - library directory" FORCE)
 
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(bullet3 REQUIRED_VARS BULLET3_INCLUDE_DIRS BULLET3_LIBRARY_DIRS)
