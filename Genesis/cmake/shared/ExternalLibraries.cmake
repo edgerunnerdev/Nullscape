bf5d0410ebb2680588b2a9f247039069540ae5a1
Pedro Nunes <pnunes515@protonmail.com>
2022-07-19 18:11:41 +0100

    bitsery and rpc now using vcpkg.

--- Genesis/cmake/shared/ExternalLibraries.cmake
+++ Genesis/cmake/shared/ExternalLibraries.cmake
@@ -2,21 +2,6 @@ include(ExternalProject)
 
 function(BuildExternalLibraries)
 
-  ExternalProject_Add(rpclib
-    PREFIX ${CMAKE_CURRENT_LIST_DIR}/ext/rpclib
-    GIT_REPOSITORY https://github.com/edgerunnerdev/rpclib.git
-	GIT_TAG 0de1a4f62d3e6cd4f3b2fd281044fb9f2b147267
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR> -DRPCLIB_MSVC_STATIC_RUNTIME=ON
-	UPDATE_COMMAND ""
-  )
-
-  ExternalProject_Add(bitsery
-    PREFIX ${CMAKE_CURRENT_LIST_DIR}/ext/bitsery
-    GIT_REPOSITORY https://github.com/fraillt/bitsery.git
-	GIT_TAG v5.2.2
-    UPDATE_COMMAND "" CONFIGURE_COMMAND "" BUILD_COMMAND "" INSTALL_COMMAND ""
-  )
-
   ExternalProject_Add(MagicEnum
     PREFIX ${CMAKE_CURRENT_LIST_DIR}/ext/MagicEnum
     GIT_REPOSITORY https://github.com/Neargye/magic_enum.git
--- Genesis/cmake/shared/Findbitsery.cmake
+++
@@ -1,4 +0,0 @@
-set(BITSERY_INCLUDE_DIRS "${CMAKE_SOURCE_DIR}/Genesis/ext/bitsery/src/bitsery/include")
-
-include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(bitsery REQUIRED_VARS BITSERY_INCLUDE_DIRS)
